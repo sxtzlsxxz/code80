30c62ef63a257bff0540a03dde04d06736435ad0
sxtzlsxxz <168646786+sxtzlsxxz@users.noreply.github.com>
2024-05-01 17:02:43 +0300

    Add files via upload

---
+++ ConsoleApplication33.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n = 10;
+    int sum = n * (n + 1) / 2;
+    cout << "Сумма чисел от 1 до 10: " << sum << endl;
+    return 0;
+}
