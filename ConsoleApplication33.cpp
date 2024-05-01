41908ef55222e4a2ad30c4bb22723e6149234c08
sxtzlsxxz <168646786+sxtzlsxxz@users.noreply.github.com>
2024-05-01 17:04:25 +0300

    Add files via upload

--- ConsoleApplication33.cpp
+++ ConsoleApplication33.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    int n = 10;
+    int n = 8;
     int sum = n * (n + 1) / 2;
-    cout << "Сумма чисел от 1 до 10: " << sum << endl;
+    cout << "Сумма чисел от 1 до 8: " << sum << endl;
     return 0;
 }
